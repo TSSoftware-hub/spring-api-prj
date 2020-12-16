9020a815d3373f60b6d2e5c08bce923028b8db5a
TSSoftware-hub <sergey.trukhanov@outlook.com>
2020-12-15 17:16:13 -0800

    Added ToDoEntity.

---
+++ src/main/java/com/tssoftwareusa/springapi/entity/ToDoEntity.java
@@ -0,0 +1,51 @@
+package com.tssoftwareusa.springapi.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class ToDoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private boolean completed;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    public ToDoEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+}
